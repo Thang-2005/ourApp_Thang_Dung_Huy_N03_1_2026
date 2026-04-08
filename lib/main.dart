117ce71be20c2565aec5038141a6c7cc16a99091
thang <nguyenthang2611205@gmail.com>
2026-04-08 10:57:05 +0700

    update day  01

--- lib/main.dart
+++ lib/main.dart
@@ -7,77 +7,51 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter thang',
       theme: ThemeData(
-        
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
-      home: const MyHomePage(title: 'Ứng Dụng Hỗ Trợ Đặt Lịch Khám Bệnh'),
-      
+      home: const MyHomePage(
+        title: 'Ứng Dụng Hỗ Trợ Đặt Lịch Khám Bệnh',
+      ),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
+class MyHomePage extends StatelessWidget {
   const MyHomePage({super.key, required this.title});
 
   final String title;
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
-  @override
   Widget build(BuildContext context) {
-    
     return Scaffold(
       appBar: AppBar(
-        
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-       
-        title: Text(widget.title),
+        title: Text(title),
       ),
       body: Center(
-  child: Column(
-    mainAxisAlignment: MainAxisAlignment.center,
-    children: [
-      const Text('You have pushed the button this many times:'),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Text(
+              'Thành viên nhóm:',
+              style: TextStyle(
+                fontSize: 18,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
 
-      Text(
-        '$_counter',
-        style: Theme.of(context).textTheme.headlineMedium,
-      ),
+            const SizedBox(height: 20),
 
-      SizedBox(height: 20),
-
-      const Text(
-        'Thành viên nhóm:',
-        style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
-      ),
-
-      const Text('1. Nguyễn Văn Thắng MSSV: 23010234'),
-      const Text('2. Dương Trí Dũng MSSV: 23010005'),
-      const Text('3. Bùi Quang Huy MSSV: 23010865'),
-    ],
-  ),
-),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
+            const Text('1. Nguyễn Văn Thắng - MSSV: 23010234'),
+            const Text('2. Dương Trí Dũng - MSSV: 23010005'),
+            const Text('3. Bùi Quang Huy - MSSV: 23010865'),
+          ],
+        ),
       ),
     );
   }
